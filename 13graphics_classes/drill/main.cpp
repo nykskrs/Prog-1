dd29cf54b0de3ecb4f2067861e79bf0eef43320c
nykskrs <71765217+nykskrs@users.noreply.github.com>
2022-03-03 09:34:28 +0100

    Update main.cpp

--- 13graphics_classes/drill/main.cpp
+++ 13graphics_classes/drill/main.cpp
@@ -3,8 +3,6 @@
 */
 #include "Simple_window.h"
 #include "Graph.h"
-#include "random"
-#include "iostream"
 
 // using namespace Graph_lib;
 
@@ -24,15 +22,15 @@ try
     for (int y = yGrid; y < ySize; y += yGrid)
         grid.add(Point(0, y), Point(xSize, y));
 
-    //diagonal red squares
-    Rectangle rect01(Point(0,0),100,100);
-    Rectangle rect02(Point(100,100),100,100);
-    Rectangle rect03(Point(200,200),100,100);
-    Rectangle rect04(Point(300,300),100,100);
-    Rectangle rect05(Point(400,400),100,100);
-    Rectangle rect06(Point(500,500),100,100);
-    Rectangle rect07(Point(600,600),100,100);
-    Rectangle rect08(Point(700,700),100,100);
+    // diagonal red squares
+    Rectangle rect01(Point(0, 0), 100, 100);
+    Rectangle rect02(Point(100, 100), 100, 100);
+    Rectangle rect03(Point(200, 200), 100, 100);
+    Rectangle rect04(Point(300, 300), 100, 100);
+    Rectangle rect05(Point(400, 400), 100, 100);
+    Rectangle rect06(Point(500, 500), 100, 100);
+    Rectangle rect07(Point(600, 600), 100, 100);
+    Rectangle rect08(Point(700, 700), 100, 100);
 
     rect01.set_fill_color(Color::red);
     rect02.set_fill_color(Color::red);
@@ -43,26 +41,20 @@ try
     rect07.set_fill_color(Color::red);
     rect08.set_fill_color(Color::red);
 
-    //4 imgs random pos
-    Image img01(Point(0,500), "chad_cat.jpg");
-    img01.set_mask(Point(60,50),200,200);
-    Image img02(Point(0,200), "chad_cat.jpg");
-    img02.set_mask(Point(60,50),200,200);
-    Image img03(Point(600,200), "chad_cat.jpg");
-    img03.set_mask(Point(60,50),200,200);
-    Image img04(Point(400,600), "chad_cat.jpg");
-    img04.set_mask(Point(60,50),200,200);
+    // 3 imgs random posit
+    Image img01(Point(0, 500), "chad_cat.jpg");
+    img01.set_mask(Point(60, 50), 200, 200);
+    Image img02(Point(0, 200), "chad_cat.jpg");
+    img02.set_mask(Point(60, 50), 200, 200);
+    Image img03(Point(600, 200), "chad_cat.jpg");
+    img03.set_mask(Point(60, 50), 200, 200);
 
-    //moving img
-    // std::random_device rd;
-    // mt19937 gen(rd());
-    // std::uniform_int_distribution<> dist(0,800);
-
+    Image img100px(Point(500, 400), "chad_cat.jpg");
+    img100px.set_mask(Point(110, 70), 100, 100);
 
     win.attach(img01);
     win.attach(img02);
     win.attach(img03);
-    win.attach(img04);
 
     win.attach(rect01);
     win.attach(rect02);
@@ -72,8 +64,23 @@ try
     win.attach(rect06);
     win.attach(rect07);
     win.attach(rect08);
-    
+
     win.attach(grid);
+
+    win.attach(img100px);
+
+    int randomValue = 0;
+
+    // moving img
+    //  ----------segmentation fault(core dumped)----------------
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     randomValue = ((rand() % 8) + 1) * 100;
+    //     img100px.move(randomValue, randomValue);
+    //     win.wait_for_button();
+    // }
+
     win.wait_for_button();
 }
 catch (exception &e)
