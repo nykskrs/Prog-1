13e9d2d3663490eae8b86eb35baf6579a70cd17b
nykskrs <71765217+nykskrs@users.noreply.github.com>
2022-03-09 16:11:49 +0100

    setup

---
+++ 15graphing_functions_and_data/main.cpp
@@ -0,0 +1,10 @@
+/*
+    g++ main.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o main `fltk-config --ldflags --use-images`
+*/
+#include "Simple_window.h"
+#include "Graph.h"
+
+int main()
+{
+    
+}
